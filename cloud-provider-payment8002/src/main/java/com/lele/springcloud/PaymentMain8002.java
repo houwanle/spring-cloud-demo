6db1c5bfe463f8f620c72519c36b291c891eebd6
houwanle <wanle@qq.com>
2021-03-06 11:56:42 +0800

    构建服务提供者 订单支付模块 集群 && 增加负载均衡（LoadBalanced）

---
+++ cloud-provider-payment8002/src/main/java/com/lele/springcloud/PaymentMain8002.java
@@ -0,0 +1,18 @@
+package com.lele.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: lele
+ * @date: 2021/2/28 20:52
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
