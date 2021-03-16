958699ff7366fdfd0fec21eae64a54bf962e38e3
houwanle <wanle@qq.com>
2021-03-16 22:17:10 +0800

    添加 手写轮询算法

--- cloud-consumer-order80/src/main/java/com/lele/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/lele/springcloud/controller/OrderController.java
@@ -2,7 +2,11 @@ package com.lele.springcloud.controller;
 
 import com.lele.springcloud.entities.CommonResult;
 import com.lele.springcloud.entities.Payment;
+import com.lele.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @author: lele
@@ -25,6 +31,12 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
@@ -45,6 +57,20 @@ public class OrderController {
         } else {
             return new CommonResult<>(444, "操作失败");
         }
+    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0) {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForObject(uri+"/payment/lb", String.class);
     }
 
 }
