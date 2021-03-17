489c3d54615fb4230681c304c69a419bd2071e12
houwanle <wanle@qq.com>
2021-03-18 07:32:13 +0800

    添加 使用 OpenFeign 的消费者模块

---
+++ cloud-consumer-feign-order80/src/main/java/com/lele/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,22 @@
+package com.lele.springcloud.service;
+
+import com.lele.springcloud.entities.CommonResult;
+import com.lele.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author: lele
+ * @date: 2021/3/17 7:31
+ * @description:
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
