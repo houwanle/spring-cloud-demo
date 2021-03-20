5bdb94540d3ed5032d087eacff677ea28fd05144
houwanle <wanle@qq.com>
2021-03-20 22:11:56 +0800

    添加 服务消费者模块 服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/lele/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lele/springcloud/controller/OrderHystrixController.java
@@ -1,12 +1,15 @@
 package com.lele.springcloud.controller;
 
 import com.lele.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: lele
@@ -27,8 +30,17 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+    @HystrixCommand(fallbackMethod = "paymentInfoFallbackMethod", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="1500")
+    })
+    public String paymentInfo_TimeOut(Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
+
+    public String paymentInfoFallbackMethod(Integer id) {
+        return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错，请检查自己，(╥╯^╰╥)";
+    }
+
+
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/lele/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lele/springcloud/service/PaymentService.java
@@ -24,21 +24,21 @@ public class PaymentService {
     }
 
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="3000")
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="5000")
     })
     public String paymentInfo_TimeOut(Integer id) {
 //        int timeNumber = 5;
-         int age = 10/0;
-//        try {
-//            TimeUnit.SECONDS.sleep(timeNumber);
-//        } catch(InterruptedException e) {
-//            e.printStackTrace();
-//        }
+//         int age = 10/0;
+        try {
+            TimeUnit.MILLISECONDS.sleep(3000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
 //        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"~~"+"耗时（秒）"+timeNumber;
-        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"~~"+"耗时（秒）";
+        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"哈哈~~"+"耗时（秒）";
     }
 
     public String paymentInfo_TimeOutHandler(Integer id) {
-        return "线程池："+Thread.currentThread().getName()+"  系统繁忙或者运行报错，请稍候再试,id: "+id+"\t"+"(╥╯^╰╥)";
+        return "线程池："+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍候再试,id: "+id+"\t"+"(╥╯^╰╥)";
     }
 }
