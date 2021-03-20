a312f71d099f91aae772459ee1272b519189cdaa
houwanle <wanle@qq.com>
2021-03-20 21:34:07 +0800

    添加 服务提供者模块 服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/lele/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lele/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.lele.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -21,13 +23,22 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName()+"  paymentInfo_OK,id:" + id+"\t"+"哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="3000")
+    })
     public String paymentInfo_TimeOut(Integer id) {
-        int timeNumber = 3;
-        try {
-            TimeUnit.SECONDS.sleep(timeNumber);
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"~~"+"耗时（秒）"+timeNumber;
+//        int timeNumber = 5;
+         int age = 10/0;
+//        try {
+//            TimeUnit.SECONDS.sleep(timeNumber);
+//        } catch(InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"~~"+"耗时（秒）"+timeNumber;
+        return "线程池："+ Thread.currentThread().getName()+"  paymentInfo_OK,id" + id+"\t"+"~~"+"耗时（秒）";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池："+Thread.currentThread().getName()+"  系统繁忙或者运行报错，请稍候再试,id: "+id+"\t"+"(╥╯^╰╥)";
     }
 }
