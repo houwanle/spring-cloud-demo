500777816bb3a8094da46bcba763a52d4344ee15
houwanle <wanle@qq.com>
2021-03-25 07:28:45 +0800

    添加 Gateway-Predicate的使用

---
+++ cloud-gateway-gateway9527/src/test/java/TempTest.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author: lele
+ * @date: 2021/3/24 22:26
+ * @description:
+ */
+public class TempTest {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
