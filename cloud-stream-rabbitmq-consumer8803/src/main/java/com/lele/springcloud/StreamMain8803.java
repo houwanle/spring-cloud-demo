23ae363e0a2a89796f3d662bb9bdfbe5209bc3a6
houwanle <wanle@qq.com>
2021-04-04 14:02:14 +0800

    添加 消息驱动之消费者模块

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/lele/springcloud/StreamMain8803.java
@@ -0,0 +1,16 @@
+package com.lele.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: lele
+ * @date: 2021/4/4 13:15
+ * @description:
+ */
+@SpringBootApplication
+public class StreamMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMain8803.class, args);
+    }
+}
