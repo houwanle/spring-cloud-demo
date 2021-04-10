164a2c230220b14f7b37b2f9c30315f3462e8646
houwanle <wanle@qq.com>
2021-04-10 15:01:09 +0800

    添加 Sentinel降级规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/lele/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lele/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.lele.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: lele
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -19,5 +23,32 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+
+        log.info("testE 测试 异常比例");
+        int age = 10/0;
+        return "------testE";
+    }
+
+    @GetMapping("/testF")
+    public String testF() {
+
+        log.info("testF 测试 异常比例");
+        int age = 10/0;
+        return "------testF";
     }
 }
